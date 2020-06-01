3869f92789c0dce0af27a4aab4be67e9af0320c1
Diego Nóbrega <diegoandradenobrega@gmail.com>
2020-06-01 22:14:41 +0100

    #4 - [0.0.0] - Create the tree for testing.

---
+++ src/test/java/com/miuey/happytree/Directory.java
@@ -0,0 +1,77 @@
+package com.miuey.happytree;
+
+/**
+ * A class that represents a directory inside of the Operation System.
+ * 
+ * <p>A <i>model</i> class used by the tests cases. Each <i>directory</i>
+ * represents conceptually a folder in OS. It is just a example to demonstrate
+ * a tree behavior and a directory schema is one the best way to do this.</p>
+ * 
+ * <p>Any class with this behavior can be used in HappyTree API.</p>
+ * 
+ * <p>Examples of interesting classes that could be used in a real project:</p>
+ * 
+ * <ul>
+ * 	<li>Catalog</li>
+ * 	<li>Node</li>
+ * 	<li>Chart</li>
+ * 	<li>Component</li>
+ * </ul>
+ * 
+ * <p>These objects can easily represent a hierarchical tree structure through
+ * the conceptual nature of these objects.</p>
+ * 
+ * @author Diego Nóbrega
+ * @author Miuey
+ *
+ */
+public class Directory {
+
+	/*
+	 * The id of this folder.
+	 */
+	private long identifier;
+	
+	/*
+	 * The id of the folder with this one will be inside of.
+	 */
+	private long parentIdentifier;
+	
+	/*
+	 * The name of the folder to be easy to identify.
+	 */
+	private String name;
+	
+	
+	public Directory(long identifier, long parentIdentifier, String name) {
+		super();
+		this.identifier = identifier;
+		this.parentIdentifier = parentIdentifier;
+		this.name = name;
+	}
+
+	
+	public long getIdentifier() {
+		return identifier;
+	}
+	
+	public void setIdentifier(long identifier) {
+		this.identifier = identifier;
+	}
+	
+	public long getParentIdentifier() {
+		return parentIdentifier;
+	}
+	
+	public void setParentIdentifier(long parentIdentifier) {
+		this.parentIdentifier = parentIdentifier;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+}
