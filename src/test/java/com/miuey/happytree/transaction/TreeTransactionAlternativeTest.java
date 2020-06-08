b4730954160839a89a21e2c6e144c9557ffaf8a3
Diego Nóbrega <diegoandradenobrega@gmail.com>
2020-06-08 15:24:10 +0100

    #8 - [0.0.1] - TreeTransaction.sessionCheckout().

--- src/test/java/com/miuey/happytree/transaction/TreeTransactionAlternativeTest.java
+++ src/test/java/com/miuey/happytree/transaction/TreeTransactionAlternativeTest.java
@@ -28,43 +28,6 @@ import com.miuey.happytree.exception.TreeException;
 public class TreeTransactionAlternativeTest {
 
 	/**
-	 * Test for the {@link TreeTransaction#sessionCheckout(String)}.
-	 * 
-	 * <p>Alternative scenario for this operation when the session is not
-	 * initialized/existed.</p>
-	 * 
-	 * <p><b>Test:</b></p>
-	 * Try to checkout an inexistent session.
-	 * <p><b>Expected:</b></p>
-	 * Receive a <code>null</code> session value when checking out an inexistent
-	 * session.
-	 * <p><b>Steps:</b></p>
-	 * <ol>
-	 * 	<li>Create two variables with the different identifiers values;</li>
-	 * 	<li>Get the transaction;</li>
-	 * 	<li>Initialize a new session with the first identifier;</li>
-	 * 	<li>Execute the {@link TreeTransaction#sessionCheckout(String)} using
-	 * 	the second identifier (inexistent);</li>
-	 * 	<li>Assert the <code>null</code> value for the session returned.</li>
-	 * </ol>
-	 * 
-	 * @throws TreeException
-	 */
-	@Test
-	public void sessionCheckout_noInitializedSession() throws TreeException {
-		final String nameTree = "sessionCheckout_noInitializedSession";
-		final String inexistentSession = "foo";
-		
-		TreeManager manager = HappyTree.createTreeManager();
-		TreeTransaction transaction = manager.getTransaction();
-		
-		transaction.initializeSession(nameTree, Directory.class);
-		TreeSession session = transaction.sessionCheckout(inexistentSession);
-		
-		assertNull(session);
-	}
-	
-	/**
 	 * Test for the {@link TreeTransaction#destroySession(String)}.
 	 * 
 	 * <p>Alternative scenario for this operation when trying to destroy a
@@ -257,5 +220,42 @@ public class TreeTransactionAlternativeTest {
 		
 		List<TreeSession> sessions = transaction.sessions();
 		assertEquals(noSession, sessions.size());
+	}
+	
+	/**
+	 * Test for the {@link TreeTransaction#sessionCheckout(String)}.
+	 * 
+	 * <p>Alternative scenario for this operation when the session is not
+	 * initialized/existed.</p>
+	 * 
+	 * <p><b>Test:</b></p>
+	 * Try to checkout an inexistent session.
+	 * <p><b>Expected:</b></p>
+	 * Receive a <code>null</code> session value when checking out an inexistent
+	 * session.
+	 * <p><b>Steps:</b></p>
+	 * <ol>
+	 * 	<li>Create two variables with the different identifiers values;</li>
+	 * 	<li>Get the transaction;</li>
+	 * 	<li>Initialize a new session with the first identifier;</li>
+	 * 	<li>Execute the {@link TreeTransaction#sessionCheckout(String)} using
+	 * 	the second identifier (inexistent);</li>
+	 * 	<li>Assert the <code>null</code> value for the session returned.</li>
+	 * </ol>
+	 * 
+	 * @throws TreeException
+	 */
+	@Test
+	public void sessionCheckout_noInitializedSession() throws TreeException {
+		final String nameTree = "sessionCheckout_noInitializedSession";
+		final String inexistentSession = "foo";
+		
+		TreeManager manager = HappyTree.createTreeManager();
+		TreeTransaction transaction = manager.getTransaction();
+		
+		transaction.initializeSession(nameTree, Directory.class);
+		TreeSession session = transaction.sessionCheckout(inexistentSession);
+		
+		assertNull(session);
 	}
 }
